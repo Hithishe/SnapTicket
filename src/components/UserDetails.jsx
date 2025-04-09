fb46490c01ed6f7d5e0ab444eb527084b557d985
Hithishe <rhithishe@gmail.com>
2025-04-09 21:26:40 +0530

    firebase route resolved

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
-import { db } from "./firebaseconfig";
+import { db } from "../firebaseconfig";
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
